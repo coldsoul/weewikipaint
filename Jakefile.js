b7a75d3d9bfd468bd0f2e49da6f50a19e2cb410b
Radi Radichev <radi.radichev@gmail.com>
2014-05-28 00:08:55 +0100

    1089214-72138342 As developer I want CI to fail on linting problems

--- Jakefile.js
+++ Jakefile.js
@@ -11,7 +11,9 @@
         var files = new jake.FileList();
         files.include("**/*.js");
         files.exclude("node_modules");
-        lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+
+        var passed = lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+        if(!passed) fail("Linting failed!");
     });
 
     desc("Integration");
